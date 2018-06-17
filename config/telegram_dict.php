61800f75d88451925679c2ad589ae614cd985281
Ilyamander los Ilyatos <mrend@icloud.com>
2018-06-17 23:14:01 +0300

    dict for TG commands

---
+++ config/telegram_dict.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    '/start' => 'Добро пожаловать! Подпишетесь на уведомления?',
+    '/inline' => '*bold text*
+                _italic text_
+                [inline URL](http://www.example.com/)
+                [inline mention of a user](tg://user?id=123456789)
+                `inline fixed-width code`
+                ```block_language
+                pre-formatted fixed-width code block
+                ```',
+
+];

f923e30ae40468017212da017542750f36a9c8b1
Ilyamander los Ilyatos <mrend@icloud.com>
2018-06-18 11:18:56 +0300

    minor

--- app/config/telegramDictionary.php
+++ app/config/telegramDictionary.php
@@ -2,13 +2,5 @@
 
 return [
     '/start' => 'Добро пожаловать! Подпишетесь на уведомления?',
-    '/inline' => "*bold text*
-                _italic text_
-                [inline URL](http://www.example.com/)
-                [inline mention of a user](tg://user?id=123456789)
-                `inline fixed-width code`
-                ```block_language
-                pre-formatted fixed-width code block
-                ```",
-
+    '/inline' => "Встроенная в сообщение клавиатура.",
 ];

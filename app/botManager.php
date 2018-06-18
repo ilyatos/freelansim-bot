d53c1282a2cdfeb564a7dbdfc840718117428073
Ilyamander los Ilyatos <mrend@icloud.com>
2018-06-18 11:19:12 +0300

    new awesome bot manager

--- app/botManager.php
+++ app/botManager.php
@@ -4,8 +4,6 @@ namespace app;
 
 require __DIR__.'/../vendor/autoload.php';
 
-use Longman\TelegramBot\Request;
-
 use app\components\bot\TelegramUpdatesManager;
 use app\components\common\Logger;
 
@@ -13,19 +11,21 @@ $updater = new TelegramUpdatesManager();
 $telegramDict = require_once 'config/telegramDictionary.php';
 
 while (true) {
-    //получает данные о входящих сообщениях
-    $results = $updater->action();
+    // Получаем данные о входящих сообщениях и объект
+    $results = $updater->getUpdates();
+    $telegram = $updater->getTelegramObj();
 
-    //если ничего нет, то ничего и не делаем
+    // Если ничего нет, то ничего и не делаем
     if (!empty($results)) {
+        // Проходимся циклом по всем принятым сообщениям
+        for ($i = 0; $i < $telegram->UpdateCount(); $i++) {
+            $telegram->serveUpdate($i);
 
-        //проходимся циклом по всем принятым сообщениям
-        foreach ($results as $result) {
-            $chatId = $result['message']['chat']['id'];
-            $message = $result['message']['text'];
+            $text = $telegram->Text();
+            $chatId = $telegram->ChatID();
 
-            //выводим пришедшее сообщение в консоль
-            Logger::log('Сообщение от chat_id = '. $chatId.' – '.$message);
+            // Выводим пришедшее сообщение в консоль
+            Logger::log('Сообщение от chat_id = ' . $chatId . ' – ' . $text);
 
             $answer = function ($message, $dict) {
                 if (array_key_exists($message, $dict)) {
@@ -35,12 +35,12 @@ while (true) {
                 }
             };
 
-            $data = [
+            $content = [
                 'chat_id' => $chatId,
-                'text' => $answer($message, $telegramDict)
+                'text' => $answer($text,$telegramDict)
             ];
-
-            Request::sendMessage($data);
+            
+            $telegram->sendMessage($content);
         }
     }
     sleep(1);

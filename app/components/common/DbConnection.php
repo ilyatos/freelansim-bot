4f871446d6888be0dd2ea7e67eecd0a7977622f5
Ilyamander los Ilyatos <mrend@icloud.com>
2018-06-20 23:56:08 +0300

    full reconstruction of the class

--- app/components/common/DbConnection.php
+++ app/components/common/DbConnection.php
@@ -6,19 +6,71 @@ use PDO;
 
 /**
  * Class DbConnection
+ *
  * @package app\components\common
  */
 class DbConnection {
+
     /**
      * @var \PDO
      */
     private $db;
 
     /**
-     * Taking te object of PDO
-     * DbConnection constructor.
+     * Adding or updating users in base.
+     *
+     * @param $id
+     * @param $subscribing
+     * @return void
      */
-    public function __construct() {
+    public function pushToBase($id, $subscribing) {
+        $sql = "UPDATE `freelansim-bot`.users SET subscribe = $subscribing WHERE chat_id = $id";
+        $condition = "SELECT count(*) FROM `freelansim-bot`.users WHERE chat_id = $id";
+        try {
+            $this->openConnection();
+            $result = $this->db->prepare($condition);
+            $result->execute();
+            $numberOfRows = $result->fetchColumn();
+            if ($numberOfRows > 0) {
+                $sql = $this->db->prepare($sql);
+                $sql->execute();
+            } else {
+                $sql = "INSERT INTO `freelansim-bot`.users(chat_id,subscribe) VALUES ($id,$subscribing)";
+                $this->db->exec($sql);
+            }
+        } catch (\PDOException $e) {
+            echo $e->getMessage();
+            echo $e->getTraceAsString();
+        } finally {
+            $this->closeConnection();
+        }
+    }
+
+    /**
+     * Return numeric chat_id of users from base.
+     *
+     * @return mixed
+     */
+    public function getUsers() {
+        $sql = "SELECT chat_id FROM `freelansim-bot`.users WHERE subscribe = 1";
+        try{
+            $this->openConnection();
+            $state = $this->db->query($sql);
+            $result = $state->fetchAll(PDO::FETCH_ASSOC);
+        } catch (\PDOException $e) {
+            echo $e->getMessage();
+            echo $e->getTraceAsString();
+        } finally {
+            $this->closeConnection();
+        }
+        return $result;
+    }
+
+    /**
+     * Open connection to the database.
+     */
+    private function openConnection()
+    {
         $user_db = require __DIR__.'/../../../config/db.php';
         try {
             $this->db = new PDO("mysql:host = $user_db[0]; dbname = $user_db[1]", $user_db[2], $user_db[3]);
@@ -30,61 +82,10 @@ class DbConnection {
     }
 
     /**
-     * Adding or updating users in base
-     * @param $id
-     * @param $subscribing
-     * @return void
+     * Close connection at all.
      */
-    public function pushToBase($id, $subscribing) {
-        $sql = "UPDATE * `freelansim-bot`.users SET subcribe = $subscribing WHERE chat_id = $id";
-        $condition = "SELECT COUNT (*) FROM `freelansim-bot`.users WHERE chat_id = $id";
-
-        try {
-            $condition = $this->db->query($condition);
-        } catch (\PDOException $e) {
-            echo $e->getMessage();
-            echo $e->getTraceAsString();
-        }
-
-        if ($condition > 0) {
-            try {
-                $sql = $this->db->prepare($sql);
-                $this->db->exec($sql);
-            } catch (\PDOException $e){
-                echo $e->getMessage();
-                echo $e->getTraceAsString();
-            } finally {
-                $this->db = null;
-            }
-        }
-        else {
-            $sql = "INSERT INTO `freelansim-bot`.users(chat_id,subscribe) VALUES ($id,$subscribing)";
-            try {
-                $this->db->exec($sql);
-            } catch (\PDOException $e) {
-                echo $e->getMessage();
-                echo $e->getTraceAsString();
-            } finally {
-                $this->db = null;
-            }
-        }
-    }
-
-    /**
-     * Return numeric chat_id of users from base
-     * @return mixed
-     */
-    public function getUsers() {
-        $sql = "SELECT chat_id FROM `freelansim-bot`.users WHERE subscribe = 1";
-        try{
-            $state = $this->db->query($sql);
-            $result = $state->fetchAll(PDO::FETCH_ASSOC);
-        } catch (\PDOException $e) {
-            echo $e->getMessage();
-            echo $e->getTraceAsString();
-        } finally {
-            $this->db = null;
-        }
-        return $result;
+    private function closeConnection()
+    {
+        $this->db = null;
     }
 }
